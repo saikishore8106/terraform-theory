9df195a8591a7e8dc5f8b0040a9cfedf7fea0b91
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-08-25 12:29:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,13 @@
+
 provider "aws" {
   region = "us-east-1"
 }
 
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_instance" "demo" {
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04 in us-east-1
   instance_type = "t2.micro"
-}
 
-# S3 Bucket
-resource "aws_s3_bucket" "storage" {
-  bucket = "my-app-bucket"
-  acl    = "private"
-}
-
-# Security Group (Firewall)
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "simple-terraform-instance"
   }
 }
